7bbebbf2dcc9466e1c8a4f08e76864f73367787d
Salvo <salvatore.piemonte7@gmail.com>
2022-08-11 14:43:42 +0200

    🐛 fixed bug for sync number of students examined

--- assets/js/syncStudenti.js
+++ assets/js/syncStudenti.js
@@ -1,22 +1,22 @@
 /**
  * Sync data html with graphs
  */
- let promossi = myChartStudenti.data.datasets[0].data[0];
- let rimandati = myChartStudenti.data.datasets[0].data[1];
- 
- let numStudentiPromossi = $("#numPromossi");
- numStudentiPromossi.text(promossi);
- 
- let numStudentiRimandati = $("#numRimandati");
- numStudentiRimandati.text(rimandati);
- 
- /**
-  * Added counter animation for Tesisti's card
-  */
- 
- $(document).ready(function(){
-     $("#counter").counterUp({
-         delay: 10,
-         time: 1000
-     })
- });
+let promossi = myChartStudenti.data.datasets[0].data[0];
+let rimandati = myChartStudenti.data.datasets[0].data[1];
+
+let numStudentiPromossi = $("#numPromossi");
+numStudentiPromossi.text(promossi);
+
+let numStudentiRimandati = $("#numRimandati");
+numStudentiRimandati.text(rimandati);
+
+
+let request = {
+    "url": "/api/getCountEsame",
+    "method": "GET",
+}
+
+$.ajax(request).done(function (response) {
+    $("#counter").text(response[0].res);
+    document.querySelector(".nPrenotati").textContent = response[0].res;
+})
