02ec41cd6c36b707a1e0f49e2c372cccad2a326b
Salvo <salvatore.piemonte7@gmail.com>
2022-09-02 21:32:02 +0200

    ✨ Calcolo espressione per il campo formula

--- server/controller/esameController.js
+++ server/controller/esameController.js
@@ -1,4 +1,6 @@
 const esameModel = require("../model/esameModel");
+const math = require("mathjs");
+
 
 exports.creaEsame = (req,callback)=>{
     esameModel.creaEsame(req,function(err,result){
@@ -47,6 +49,35 @@ exports.updateEsame = (req,callback)=>{
         callback({msg: "Errore, nessun body passato!"},null);
         return;
     }
+    /**
+     * Analisi del body per il valore del campo 'Formula'
+     */
+     const formula = req.body.formula.toLowerCase();
+     let formulaNoSpace = formula.replaceAll(" ",'');
+     if(formulaNoSpace.includes("math.round(")){        //fare la prova con startsWith()
+         let exp = formulaNoSpace.substr(11,formulaNoSpace.length-12);
+         let operazione = exp[exp.length/2];
+        
+         let expLeft = exp.substr(exp.indexOf('(')+1,exp.indexOf(')')-1);
+         let expRight = exp.substr(exp.lastIndexOf('(')+1,exp.lastIndexOf(')')-9);  
+         
+         console.log("Sinistra: ",expLeft);
+         console.log("Destra: ",expRight);
+         
+         let risLeft = math.evaluate(expLeft);
+         let risRight = math.evaluate(expRight);
+         let expFinal = risLeft+operazione+risRight;
+         let votoScrittoOrale = Math.round(math.evaluate(expFinal));
+ 
+         console.log("NON AGGIORNATO: ",req.body);
+     
+         req.body.formula = formulaNoSpace;
+         req.body.votoComplessivo = votoScrittoOrale + parseInt(req.body.orale) + parseInt(req.body.laboratorio);
+         console.log("AGGIORNATO: ",req.body);
+     }
+     else{
+         req.body.formula = 'Formula non valida!';
+     }
     esameModel.updateEsame(req,function(err,result){
         if(!result){
             console.log("Errore controller: ");
