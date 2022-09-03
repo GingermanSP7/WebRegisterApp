8c98acb0e5c2c7cb016724f2ff702ec9125bf2e9
Salvo <salvatore.piemonte7@gmail.com>
2022-09-03 10:45:32 +0200

    ✨ Aggiunto un nuovo trigger per l'aggiunta al progettista alla modifica di un esame

--- server/database/triggers/triggerProgettista.sql
+++ server/database/triggers/triggerProgettista.sql
@@ -1,6 +1,19 @@
 DELIMITER !
-create trigger inserisciProgettista 
+create trigger inserisciProgettistaAfterUpdate 
 after update on esame 
+for each row
+BEGIN
+	if(new.stato = "Progetto" or new.stato = "progetto") then
+		insert into progettista(idAppello,matricola,voto,votoProgetto,votoFinale,dataConsegna,esito,titoloProgetto) 
+		values (new.idAppello,new.matricola,new.votoComplessivo,0,'-','-','-','-'); 
+	end if;
+END; !
+DELIMITER ;
+
+
+DELIMITER !
+create trigger inserisciProgettista 
+after insert on esame 
 for each row
 BEGIN
 	if(new.stato = "Progetto" or new.stato = "progetto") then
